5d9bcaaa272bcdf34a9093a5ebca73350ce48fa9
Andres Mallea <andresmallea3@gmail.com>
2026-06-19 02:24:44 -0400

    tdd2-rojo

---
+++ tests/Telefone.test.js
@@ -0,0 +1,8 @@
+import { Appointment } from '../src/models/Appointment';
+    test('isValidPhone debe retornar true solo si empieza con 6 o 7 y tiene 8 dígitos', () => {
+        const apptValido = new Appointment({ patient_phone: '71234567' });
+        const apptInvalido = new Appointment({ patient_phone: '12345' });
+        
+        expect(apptValido.isValidPhone()).toBe(true);
+        expect(apptInvalido.isValidPhone()).toBe(false);
+    });
