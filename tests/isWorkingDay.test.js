4d44c3f4e4d45d733efe4ac1287ce19ede1b188e
Andres Mallea <andresmallea3@gmail.com>
2026-06-19 03:57:48 -0400

    Nuevo test validacion de validacion de dias de trabajo

---
+++ tests/isWorkingDay.test.js
@@ -0,0 +1,6 @@
+import { DateUtils } from '../src_1/src/utils/DateUtils';
+test('isWorkingDay', () => {
+    const fechaDomingo = '2026-05-31T09:00:00Z'; 
+    const result = DateUtils.isWorkingDay(fechaDomingo);
+    expect(result).toBe(false);
+});
