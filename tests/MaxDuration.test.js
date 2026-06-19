e3ea080d3039e61f4ead23451b29638be3f4b5c5
Andres Mallea <andresmallea3@gmail.com>
2026-06-19 03:54:29 -0400

    Nuevo test validacion de telefono

--- tests/MaxDuration.test.js
+++ tests/MaxDuration.test.js
@@ -2,7 +2,6 @@ import { Appointment } from '../src_1/src/models/Appointment';
 
 test('isValidDuration debe retornar false si la cita dura más de 120 minutos', () => {
     const appt = new Appointment({});
-    // 3 horas de diferencia (10:00 a 13:00)
     const isValid = appt.isValidDuration('2026-06-01T10:00:00', '2026-06-01T13:00:00');
     expect(isValid).toBe(false);
 });
---
+++ tests/isValidPhone.test.js
@@ -0,0 +1,9 @@
+import { Appointment } from '../src_1/src/models/Appointment';
+test('isValidPhone_DebeRetornarTrue_SoloConPrefijosBolivianos', () => {
+    const apptBolivia = new Appointment({ patient_phone: '77654321' });
+    const apptFalso = new Appointment({ patient_phone: '12345678' }); 
+    const resultBolivia = apptBolivia.isValidPhone();
+    const resultFalso = apptFalso.isValidPhone();
+    expect(resultBolivia).toBe(true);
+    expect(resultFalso).toBe(false);
+});
